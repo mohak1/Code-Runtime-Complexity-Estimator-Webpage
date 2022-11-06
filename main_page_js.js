72336b459f03c5344e97d375a928b60e6092531a
mohak <narangmohak9@gmail.com>
2022-11-06 20:36:14 +0000

    removing array/list option for inputs

--- main_page_js.js
+++ main_page_js.js
@@ -3,10 +3,9 @@ document.getElementById('processing_image').setAttribute('hidden','');
 //disable the output graphs div on page load
 document.getElementById('output_graphs').setAttribute('hidden','');
 //for displaying google graphs
-// google.load('visualization', '1', {packages: ['corechart']});
-
+google.load("visualization", "1", {packages: ["corechart"]});
 function drawChart(tab, name){
-    var array = google.visualization.arrayToDataTable(tab);
+    var array = google.visualization.arrayToDataTable(twoDArray=tab, opt_firstRowIsData=false);
     var options = {
         title: name+' Function',
         'width':900,
@@ -56,7 +55,6 @@ var try_fibonacci_button = document.getElementById('try_fibonacci');
 var submit_button = document.getElementById('submit_button');
 var string_radio = document.getElementById('string');
 var num_radio = document.getElementById('number');
-var array_radio = document.getElementById('array');
 
 var num_type_label = document.getElementById('num_types_allowed');
 var int_box = document.getElementById('int');
@@ -78,14 +76,6 @@ var upper_az_box = document.getElementById('upper_az');
 var upper_az_label = document.getElementById('upper_az_label');
 var num_box = document.getElementById('nums');
 var num_box_label = document.getElementById('nums_label');
-
-var arr_ele_label = document.getElementById('arr_ele_allowed_label');
-var int_radio = document.getElementById('int_radio')
-var int_radio_label = document.getElementById('int_radio_label')
-var float_radio = document.getElementById('float_radio')
-var float_radio_label = document.getElementById('float_radio_label')
-var str_radio = document.getElementById('str_radio');
-var str_radio_label = document.getElementById('str_radio_label');
 
 var br1 = document.getElementById('inter_break_1');
 var br2 = document.getElementById('inter_break_2');
@@ -114,13 +104,6 @@ function hide_all_sub_selections(){
     range_start.style.display = 'none';
     range_end_label.style.display = 'none';
     range_end.style.display = 'none';
-    arr_ele_label.style.display = 'none';
-    int_radio.style.display = 'none';
-    int_radio_label.style.display = 'none';
-    float_radio.style.display = 'none';
-    float_radio_label.style.display = 'none';
-    str_radio.style.display = 'none';
-    str_radio_label.style.display = 'none';
     br1.style.display = 'none';
     br2.style.display = 'none';
     br3.style.display = 'none';
@@ -208,85 +191,6 @@ num_radio.onclick = function(){
     br4.style.display = 'inline';
 }
 
-array_radio.onclick = function(){
-    // display the sub-selections for string; hide all others
-    hide_all_sub_selections();
-    arr_ele_label.style.display = 'inline';
-    int_radio.style.display = 'inline';
-    int_radio_label.style.display = 'inline';
-    float_radio.style.display = 'inline';
-    float_radio_label.style.display = 'inline';
-    str_radio.style.display = 'inline';
-    str_radio_label.style.display = 'inline';
-}
-
-str_radio.onclick = function(){
-    // hide elements of int/float
-    range_start.style.display = 'none';
-    range_start_label.style.display = 'none';
-    range_end.style.display = 'none';
-    range_end_label.style.display = 'none';
-    //show elements of str
-    allowed_chars_label.style.display = 'inline';
-    lower_az_box.style.display = 'inline';
-    lower_az_label.style.display = 'inline';
-    upper_az_box.style.display = 'inline';
-    upper_az_label.style.display = 'inline';
-    num_box.style.display = 'inline';
-    num_box_label.style.display = 'inline';
-    str_max_len_label.style.display = 'inline';
-    str_len_input.style.display = 'inline';
-    br1.style.display = 'inline';
-    br2.style.display = 'inline';
-    br3.style.display = 'inline';
-    br4.style.display = 'inline';
-}
-
-float_radio.onclick = function(){
-    // hide elements of string
-    allowed_chars_label.style.display = 'none';
-    lower_az_box.style.display = 'none';
-    lower_az_label.style.display = 'none';
-    upper_az_box.style.display = 'none';
-    upper_az_label.style.display = 'none';
-    num_box.style.display = 'none';
-    num_box_label.style.display = 'none';
-    str_max_len_label.style.display = 'none';
-    str_len_input.style.display = 'none';
-    br1.style.display = 'none';
-    br2.style.display = 'none';
-    // show elements of float
-    range_start.style.display = 'inline';
-    range_start_label.style.display = 'inline';
-    range_end.style.display = 'inline';
-    range_end_label.style.display = 'inline';
-    br3.style.display = 'inline';
-    br4.style.display = 'inline';
-}
-
-int_radio.onclick = function(){
-    // hide elements of string
-    allowed_chars_label.style.display = 'none';
-    lower_az_box.style.display = 'none';
-    lower_az_label.style.display = 'none';
-    upper_az_box.style.display = 'none';
-    upper_az_label.style.display = 'none';
-    num_box.style.display = 'none';
-    num_box_label.style.display = 'none';
-    str_max_len_label.style.display = 'none';
-    str_len_input.style.display = 'none';
-    br1.style.display = 'none';
-    br2.style.display = 'none';
-    // show elements of int
-    range_start.style.display = 'inline';
-    range_start_label.style.display = 'inline';
-    range_end.style.display = 'inline';
-    range_end_label.style.display = 'inline';
-    br4.style.display = 'inline';
-    br3.style.display = 'inline';
-    
-}
-
 // send errors (discord?)
 function send_error(error_message){
     alert('NotImplementedError: `send_error` is not implemented');
@@ -322,7 +226,7 @@ document.getElementById('try_fibonacci').onclick = function(){
     // set input type
     num_radio.checked = true;
     string_radio.disabled = true;
-    array_radio.disabled = true;
+    // array_radio.disabled = true;
     num_type_label.style.display = 'inline';
     int_box.style.display = 'inline';
     int_label.style.display = 'inline';
@@ -425,78 +329,6 @@ document.getElementById('submit_button').onclick = function(){
             "range_start": range_start_value,
             "range_end": range_end_value
         }
-    }else if (array_radio.checked){
-        input_type = 2
-        var element_type = -1;
-        var range_start_value;
-        var range_end_value;
-        var allowed_chars_bin;
-        var max_length_value;
-        if (int_radio.checked){
-            element_type = 0;
-            if (isNaN(parseInt(range_start.value))){
-                alert('"Range start" field expects a numeric value');
-                return false;
-            }
-            if (isNaN(parseInt(range_end.value))){
-                alert('"Range end" field expects a numeric value');
-                return false;
-            }
-            range_start_value = parseInt(range_start.value);
-            range_end_value = parseInt(range_end.value);
-            
-            if (range_start_value > range_end_value){
-                alert('"Range end" cannot be less than "Range start"');
-                return;
-            }
-        }else if (float_radio.checked){
-            element_type = 1;
-            if (isNaN(parseFloat(range_start.value))){
-                alert('"Range start" field expects a numeric value');
-                return false;
-            }
-            if (isNaN(parseFloat(range_end.value))){
-                alert('"Range end" field expects a numeric value');
-                return false;
-            }
-            range_start_value = parseFloat(range_start.value);
-            range_end_value = parseFloat(range_end.value);
-            
-            if (range_start_value > range_end_value){
-                alert('"Range end" cannot be less than "Range start"');
-                return;
-            }
-        }else if (str_radio.checked){
-            element_type = 2;
-            allowed_chars_bin = [
-                lower_az_box.checked, upper_az_box.checked, num_box.checked
-            ].map(val => val ? 1:0).join('');
-    
-            if (allowed_chars_bin === '000'){
-                alert('Please select the characters allowed in the string');
-                return;
-            }
-    
-            if (
-                isNaN(parseInt(str_len_input.value)) || 
-                parseInt(str_len_input.value)<1
-            ){
-                alert('"Max string length" field expects an integer value>0');
-                return false;
-            }
-            max_length_value = parseInt(str_len_input.value);
-
-        }else{
-            alert('Please select the type of inputs allowed in the array');
-            return;
-        }
-        arr_details = {
-            "element_type": element_type,
-            "range_start": range_start_value,
-            "range_end": range_end_value,
-            "characters_allowed": allowed_chars_bin,
-            "max_length": max_length_value
-        }
     }else{
         alert('Please select the type of input your code accepts');
         return;
@@ -513,9 +345,10 @@ document.getElementById('submit_button').onclick = function(){
         response_json["string_details"] = string_details
     }else if (input_type==1){
         response_json["number_details"] = number_details
-    }else if (input_type==2){
-        response_json["array_details"] = arr_details
     }
+    
+    // show processing gif
+    // document.getElementById("processing_image").removeAttribute("hidden");
 
     // cors: https://stackoverflow.com/a/59353387
     fetch(BASE_URL + 'estimate_complexity', {
@@ -529,7 +362,125 @@ document.getElementById('submit_button').onclick = function(){
         body: JSON.stringify(response_json)
     }).then( (response) => { 
         // check if the POST returns a status code other than 200
-     });  
+        if (response.status==200){
+            // hide the processing gif
+            // document.getElementById("processing_image").setAttribute("hidden");
+            return response.json()
+        }
+    }).then((response_json) => {
+        //show the results and plot the graphs
+        var estimatedComplexity = response_json['estimatedComplexity'];
+        var runtimeList = response_json["runtime_list"];
+        var constantModelParams = response_json["constant_model"];
+        var linearModelParams = response_json["linear_model"];
+        var logModelParams = response_json["log_model"];
+        var quasiModelParams = response_json["quasi_model"];
+        var quadModelParams = response_json["quadratic_model"];
+        var expModelParams = response_json["exponential_model"];
+        
+        //constant model
+        function constantModel(paramList, x) {
+            a = paramList[0];
+            return (x*0)+a;
+        }
+        //log model
+        function logModel(paramList, x) {
+            a = paramList[0];
+            b = paramList[1];
+            return (a*Math.log(x)) + b;
+        }
+        //linear model
+        function linearModel(paramList, x) {
+            a = paramList[0];
+            b = paramList[1];
+            return (a*x)+b;
+        }
+        //quasilinear model
+        function quasiModel(paramList, x) {
+            a = paramList[0];
+            b = paramList[1];
+            return (a*x*Math.log(x))+b;
+        }
+        //quadratic model
+        function quadModel(paramList, x) {
+            a = paramList[0];
+            b = paramList[1];
+            c = paramList[2];
+            return (a*x*x)+(b*x)+c;
+        }
+        //exponential model
+        function expModel(paramList, x) {
+            a = paramList[0];
+            b = paramList[1];
+            return Math.pow(2, ((a*x)+b));
+        }
+
+        //display the runtime complexity
+        document.getElementById("final_complexity").innerHTML = estimatedComplexity;
+        //google data array for constant plot
+        var constantPlot = [['X', 'Points', 'Constant Line']];
+        for(var i=1; i<=runtimeList.length; i++){
+            var constantModelValue = constantModel(constantModelParams, i);
+            var newRow = [i, runtimeList[i-1], constantModelValue];
+            constantPlot.push(newRow);
+        }
+        
+        //google data array for log plot
+        var logPlot = [['X', 'Points', 'Logarithmic Curve']];
+        for(var i=1; i<=runtimeList.length; i++){
+            var logModelValue = logModel(logModelParams, i);
+            var newRow = [i, runtimeList[i-1], logModelValue];
+            logPlot.push(newRow);
+        }
+        
+        //google data array for linear plot
+        var linearPlot = [['X', 'Points', 'Linear Line']];
+        for(var i=1; i<=runtimeList.length; i++){
+            var linearModelValue = linearModel(linearModelParams, i);
+            var newRow = [i, runtimeList[i-1], linearModelValue];
+            linearPlot.push(newRow);
+        }
+        
+        //google data array for quasilinear plot
+        var quasiPlot = [['X', 'Points', 'Quasilinear Curve']];
+        for(var i=1; i<=runtimeList.length; i++){
+            var quasiModelValue = quasiModel(quasiModelParams, i);
+            var newRow = [i, runtimeList[i-1], quasiModelValue];
+            quasiPlot.push(newRow);
+        }
+        
+        //google data array for quadratic plot
+        var quadPlot = [['X', 'Points', 'Quadratic Curve']];
+        for(var i=1; i<=runtimeList.length; i++){
+            var quadModelValue = quadModel(quadModelParams, i);
+            var newRow = [i, runtimeList[i-1], quadModelValue];
+            quadPlot.push(newRow);
+        }
+        
+        //google data array for exponential plot
+        var expPlot = [['X', 'Points', 'Exponential Curve']];
+        for(var i=1; i<=runtimeList.length; i++){
+            var expModelValue = expModel(expModelParams, i);
+            var newRow = [i, runtimeList[i-1], expModelValue];
+            expPlot.push(newRow);
+        }
+    
+        //draw all the plots 
+        drawChart(constantPlot, "Constant");
+        drawChart(logPlot, "Logarithmic");
+        drawChart(linearPlot, "Linear");
+        drawChart(quasiPlot, "Quasilinear");
+        drawChart(quadPlot, "Quadratic");
+        drawChart(expPlot, "Exponential");
+        
+        //make them visible
+        document.getElementById("output_graphs").removeAttribute("hidden");
+        
+        //smoothly scroll to the plotted graphs div
+        var elmntToView = document.getElementById("output_graphs");
+        elmntToView.scrollIntoView({behavior: "smooth"});
+
+    });
 };
 
 
